c4a3a805c15d13d9d4a2aff89dfaa453fb74f71c
Octavio orlando OJEDA ARONIZ <ocojeda-@e2r13p3.42.fr>
2017-02-18 19:23:59 +0100

    couilles!

--- line.c
+++ line.c
@@ -6,20 +6,45 @@
 /*   By: ocojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 17:58:06 by ocojeda-          #+#    #+#             */
-/*   Updated: 2017/02/18 14:25:03 by tfaure           ###   ########.fr       */
+/*   Updated: 2017/02/18 19:23:15 by ocojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static t_line	*ft_newline(t_point *pointa, t_point *pointb, t_line *new)
+void	ft_setpoint(t_point *pointa, t_point *pointb, t_screen *fst, t_line *new)
 {
+	if(pointa->x < 0)
+		new->x0 = 0;
+	else if(pointa->x > fst->len)
+		new->x0 = fst->len;
+	else
+		new->x0 = pointa->x;
+	if(pointb->x < 0)
+		new->x1 = 0;
+	else if(pointb->x > fst->len)
+		new->x1 = fst->len;
+	else
+		new->x1 = pointb->x;
+	if(pointa->y < 0)
+		new->y0 = 0;
+	else if(pointa->y > fst->hight)
+		new->y0 = fst->hight;
+	else
+		new->y0 = pointa->y;
+	if(pointb->y < 0)
+		new->y1 = 0;
+	else if(pointb->y > fst->hight)
+		new->y1 = fst->hight;
+	else
+		new->y1 = pointb->y;
+}
+static t_line	*ft_newline(t_point *pointa, t_point *pointb, t_line *new, t_screen*fst)
+{
+	
 	if (!(new = (t_line *)malloc(sizeof(t_line))))
 		return (NULL);
-	new->x0 = pointa->x;
-	new->y0 = pointa->y;
-	new->x1 = pointb->x;
-	new->y1 = pointb->y;
+	ft_setpoint(pointa, pointb, fst, new);
 	new->dx = new->x1 - new->x0;
 	new->dy = new->y1 - new->y0;
 	if (new->dy < 0)
@@ -90,14 +115,25 @@ t_point			*new_point(float x, float y, float z, unsigned int color)
 	new->nexty = NULL;
 	return (new);
 }
-
+int				ft_boundaries(t_point *pointa, t_point *pointb, int len, int hight)
+{
+	if(pointa->x < 0 && pointb->x < 0)
+		return 0;
+	if(pointa->y < 0 && pointb->y < 0)
+		return 0;
+	if(pointa->x > len && pointb->x > len)
+		return 0;
+	if(pointa->y > hight && pointb->y > hight)
+		return 0;
+	return 1;
+}
 void			ft_put_diagonal(t_point *pointa, t_point *pointb, t_screen *fst)
 {
 	t_line		*line;
 
-	if (pointa && pointb)
+	if (pointa && pointb && ft_boundaries(pointa, pointb, fst->len, fst->hight))
 	{
-		line = ft_newline(pointb, pointa, NULL);
+		line = ft_newline(pointb, pointa, NULL, fst);
 		if (!ft_steps(line, fst, pointb->color))
 		{
 			while (line->y != line->y1)
