d1370f86685774f9291abc00a5b25922211ab518
Mathilde YERNAUX <myernaux@e2r13p2.42.fr>
2017-02-27 12:04:38 +0100

    bite

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: ocojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 10:30:11 by ocojeda-          #+#    #+#             */
-/*   Updated: 2017/02/27 10:40:42 by ocojeda-         ###   ########.fr       */
+/*   Updated: 2017/02/27 12:04:18 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@
 # define R_TRANSLATE 100
 
 # define RADIANCONV 0.017453259
-
 
 typedef struct		s_point
 {
@@ -66,79 +65,49 @@ typedef struct		s_screen
 	char			*data;
 	t_point			*fp;
 	t_point			*center;
-	float				*cpy;
+	float			*cpy;
 }					t_screen;
 
-/************* MAIN.C ***************/
-t_point         *test3(t_point *fp, void (*f)(t_point *point, int angle), int angle);
-t_point			*test_rota(t_point *fp, void (*f)(t_point *point), t_point *center);
-t_point			*test2(t_point *fp, void (*f)(t_point *point, int angle), int angle);
-t_point			*test(t_point *fp, void (*f)(t_point *point));
-void			print_point(t_point *point);
-t_point			*addcenter(t_point *fp);
-
-/*********** KEYCODE.c **************/
-
-int				my_key_func(int keycode, t_screen *fst);
-
-/************* PARSE.C **************/
-
-t_point			*ft_get_map(char *str);
-int				ft_erase_map(t_point *temp, t_point *temp2, t_point *temp3);
-
-/*********** PROYECTION.C ***********/
-
-void			*ft_proyection(t_point *fp);
-void			ft_put_pix_map(t_point *fp, t_screen *fst, t_point *t, t_point *t2);
-
-/************* LINE.C ***************/
-
-void			ft_put_diagonal(t_point *pointa, t_point *pointb, t_screen *fst);
-t_point			*new_point(float x, float y, float z, unsigned int color);
-int				ft_steps(t_line *line, t_screen *fst, unsigned int col);
-t_line			*ft_newline(t_point *pa, t_point *pb, t_line *nw);
-void            ft_setpoint(t_point *pa, t_point *pb, t_line *nw);
-int				ft_steps_else(t_line *line);
-
-/*************  MAP.C  **************/
-
-void			ft_center(t_point *fp, t_point *center, int lenght, int hight);
-t_point			*ft_find_centermap(t_point *fp, float x, float x1, float y);
-int				ft_maphight(t_point *fp);
-int				ft_maplength(t_point *fp);
-
-/************* BONUS.C **************/
-
-void			ft_zoom(t_point *fp);
-void			ft_set_zoom(t_point *fp);
-
-/************* BONUS1.C *************/
-
-void			ft_zoom_out(t_point *fp);
-void			ft_higher(t_point *fp);
-
-/************ FT_ATOI_HEXA.C ********/
-
-unsigned int	ft_atoi_hexa(char *nbr);
-
-/************ matrix.c **************/
-
-void			matrice_rotate_x(t_point *temp, int angle);
-void			matrice_rotate_y(t_point *temp, int angle);
-void			matrice_rotate_z(t_point *temp, int angle);
-void			matrice_translate_ud(t_point *temp, int dist);
-void			matrice_translate_rl(t_point *temp, int dist);
-
-void			ft_resetmap(t_point *fp, float *z0);
-float			*ft_mapcpy(t_point *fp);
-
-/************ fl_to_int.c *************/
-
-int				fl_to_int(float fl);
-char			**ft_iscolor(char *str);
-
-/************ ft_error.c  ************/
-
-void			ft_error(char *str);
+t_point				*test3(t_point *fp, void (*f)(t_point *p, int gle),
+						int gle);
+t_point				*test_rota(t_point *fp, void (*f)(t_point *p),
+						t_point *center);
+t_point				*test2(t_point *fp, void (*f)(t_point *p, int gle),
+						int gle);
+t_point				*test(t_point *fp, void (*f)(t_point *point));
+void				print_point(t_point *point);
+t_point				*addcenter(t_point *fp);
+int					my_key_func(int keycode, t_screen *fst);
+t_point				*ft_get_map(char *str);
+int					ft_erase_map(t_point *temp, t_point *temp2, t_point *temp3);
+void				*ft_proyection(t_point *fp);
+void				ft_put_pix(t_point *fp, t_screen *fst, t_point *t,
+						t_point *t2);
+void				ft_put_diagonal(t_point *pa, t_point *pb, t_screen *fst);
+t_point				*new_point(float x, float y, float z, unsigned int color);
+int					ft_steps(t_line *line, t_screen *fst, unsigned int col);
+t_line				*ft_newline(t_point *pa, t_point *pb, t_line *nw);
+void				ft_setpoint(t_point *pa, t_point *pb, t_line *nw);
+int					ft_steps_else(t_line *line);
+void				ft_center(t_point *fp, t_point *center, int lenght,
+						int hight);
+t_point				*ft_find_centermap(t_point *fp, float x, float x1, float y);
+int					ft_maphight(t_point *fp);
+int					ft_maplength(t_point *fp);
+void				ft_zoom(t_point *fp);
+void				ft_set_zoom(t_point *fp);
+void				ft_zoom_out(t_point *fp);
+void				ft_higher(t_point *fp);
+unsigned int		ft_atoi_hexa(char *nbr);
+void				matrice_rotate_x(t_point *temp, int angle);
+void				matrice_rotate_y(t_point *temp, int angle);
+void				matrice_rotate_z(t_point *temp, int angle);
+void				matrice_translate_ud(t_point *temp, int dist);
+void				matrice_translate_rl(t_point *temp, int dist);
+void				ft_resetmap(t_point *fp, float *z0);
+float				*ft_mapcpy(t_point *fp);
+int					fl_to_int(float fl);
+char				**ft_iscolor(char *str);
+void				ft_error(char *str);
 
 #endif
